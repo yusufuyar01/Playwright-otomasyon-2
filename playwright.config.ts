3218b0fb9721ab5f09482542b0b5f626103469dc
Yusuf UYAR <uyaryusuf01@gmail.com>
2025-08-11 14:38:15 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  /* Global setup for API tests */
+  globalSetup: './tests/api-test/global-setup.ts',
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
